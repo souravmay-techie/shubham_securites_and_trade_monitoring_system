cb000eea3e4695aade2687beb30086d47804c496
dasbaba <souravmay.techie@gmail.com>
2025-01-17 14:51:19 +0000

    fixed rjsf dependencies issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,108 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
+//import { Form } from "@rjsf/antd";
+//import { AntdForm } from "@rjsf/antd";
+import Form from "@rjsf/bootstrap-4";
+import validator from "@rjsf/validator-ajv8";
 import "./App.css";
 
+const schema = {
+  "title": "A registration form",
+  "description": "A simple form example.",
+  "type": "object",
+  "required": [
+    "firstName",
+    "lastName"
+  ],
+  "properties": {
+    "firstName": {
+      "type": "string",
+      "title": "First name",
+      "default": "Chuck"
+    },
+    "lastName": {
+      "type": "string",
+      "title": "Last name"
+    },
+    "age": {
+      "type": "integer",
+      "title": "Age"
+    },
+    "bio": {
+      "type": "string",
+      "title": "Bio"
+    },
+    "password": {
+      "type": "string",
+      "title": "Password",
+      "minLength": 3
+    },
+    "telephone": {
+      "type": "string",
+      "title": "Telephone",
+      "minLength": 10
+    }
+  }
+};
+
+const uiSchema = {
+  "firstName": {
+    "ui:autofocus": true,
+    "ui:emptyValue": "",
+    "ui:placeholder": "ui:emptyValue causes this field to always be valid despite being required",
+    "ui:autocomplete": "family-name",
+    "ui:enableMarkdownInDescription": true,
+    "ui:description": "Make text **bold** or *italic*. Take a look at other options [here](https://markdown-to-jsx.quantizor.dev/)."
+  },
+  "lastName": {
+    "ui:autocomplete": "given-name",
+    "ui:enableMarkdownInDescription": true,
+    "ui:description": "Make things **bold** or *italic*. Embed snippets of `code`. <small>And this is a small texts.</small> "
+  },
+  "age": {
+    "ui:widget": "updown",
+    "ui:title": "Age of person",
+    "ui:description": "(earth year)"
+  },
+  "bio": {
+    "ui:widget": "textarea"
+  },
+  "password": {
+    "ui:widget": "password",
+    "ui:help": "Hint: Make it strong!"
+  },
+  "telephone": {
+    "ui:options": {
+      "inputType": "tel"
+    }
+  }
+};
+
 function App() {
+  
+  
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name }));
-  }
+  };
 
+  async function onSubmit(data) {
+    console.log(data);
+  };
+  const customValidator = (formData, schema) => {
+    const errors = {};
+    alert("Das");
+  
+ 
+  
+    return errors;
+  };
+
+  console.log("Why is this not working");
   return (
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
@@ -44,6 +135,13 @@ function App() {
         <button type="submit">Greet</button>
       </form>
       <p>{greetMsg}</p>
+      <br/>
+      <div>
+        Hello dear
+        <Form schema={schema} uiSchema = {uiSchema} onSubmit={onSubmit}  validator={customValidator}/>    
+    </div>
+    
+    
     </main>
   );
 }
